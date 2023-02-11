1ecb2dc5f37a59b4c38f49a1a1dea0de95c15267
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-11 20:53:31 +0600

    Selecting records from a table. Django

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
