08ab26ceabf8658ea35d996feaca87310604bfb2
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-11 21:11:50 +0600

    Changing and deleting entries. Django

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
