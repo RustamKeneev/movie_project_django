30f4a44a2937f5b399408984cf775519085ee1ea
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 16:27:49 +0600

    Sorting QuerySet. Order by Django

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+from django.db.models import F
 
 
 def show_all_movies(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by(F('year').desc(nulls_last=True), 'rating')
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
@@ -16,7 +17,7 @@ def show_all_movies(request):
 #         'movie': movie
 #     })
 
-def show_one_movie(request, slug_movie:str):
+def show_one_movie(request, slug_movie: str):
     # movie = Movie.objects.get(id=id_movie)
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
