6516bcfbd4665681d46dd9ff8fd7b2c32e634d3b
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 15:31:18 +0600

    Slug field. Django

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
+    slug = models.SlugField(default='', null=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Movie, self).save(*args, **kwargs)
+    #
+    # def get_url(self):
+    #     return reverse('movie-detail', args=[self.id])
 
     def get_url(self):
-        return reverse('movie-detail',args=[self.id])
+        return reverse('movie-detail', args=[self.slug])
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.show_all_movies),
-    path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
+    # path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
+    path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
 ]
