20c9f6c63710de3c8ae3dd86afe170d3061a8ff9
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 20:45:23 +0600

    Django admin panel

--- movie_app/views.py
+++ movie_app/views.py
@@ -4,15 +4,15 @@ from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 
 def show_all_movies(request):
-    # movies = Movie.objects.order_by(F('year').desc(nulls_last=True), 'rating')
-    movies = Movie.objects.annotate(
-        true_bool=Value(True),
-        false_bool=Value(False),
-        str_field=Value("Hello"),
-        int_field=Value(123),
-        new_budget=F('budget') + 100,
-        test_field=F('rating') + F('year'),
-    )
+    movies = Movie.objects.order_by(F('year').desc(nulls_last=True), 'rating')
+    # movies = Movie.objects.annotate(
+    #     true_bool=Value(True),
+    #     false_bool=Value(False),
+    #     str_field=Value("Hello"),
+    #     int_field=Value(123),
+    #     new_budget=F('budget') + 100,
+    #     test_field=F('rating') + F('year'),
+    # )
     aggregate = movies.aggregate(Sum('budget'), Avg('budget'), Avg('rating'), Min('rating'), Max('rating'), Count('id'))
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies,
