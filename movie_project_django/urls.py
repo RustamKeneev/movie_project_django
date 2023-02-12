fbab99497ec70e317912b083cbe0ed21e685849b
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 21:44:43 +0600

    Customizing Django's Main Dashboard

--- movie_project_django/urls.py
+++ movie_project_django/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = 'Наша админка'
+admin.site.index_title = 'Супер админка'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie_app.urls')),
