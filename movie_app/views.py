ef59e9f55325efbb01a5733fb09a7bd5efb2db72
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 13:37:14 +0600

    We display QuerySet in a template. Django

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def show_all_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
