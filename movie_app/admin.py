40d20032cebeb83d50850cf4d37236753f4b3612
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 22:32:04 +0600

    Calculated field in Django admin

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,10 +4,19 @@ from .models import Movie
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
     ordering = ['rating']
     list_per_page = 3
 
+    @admin.display(ordering='rating', description='Статус фильмов')
+    def rating_status(self, movie: Movie):
+        if movie.rating < 50:
+            return "Рейтинг менее 50 процентов не рекомендую"
+        if movie.rating < 70:
+            return "Рейтинг менее 70 процентов можно рекомендовать"
+        if movie.rating <= 85:
+            return "Рейтинг менее 85 процентов  рекомендую"
+        return 'Рекомендованные'
 
 # admin.site.register(Movie, MovieAdmin)
