b8a30451682b4265889062407461bb3beaac7d9a
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-12 22:07:30 +0600

    Django admin. Customizing the List Form

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Movie
 
 
-admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['rating']
+    list_per_page = 3
+
+
+# admin.site.register(Movie, MovieAdmin)
--- movie_app/models.py
+++ movie_app/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
-    year = models.IntegerField(null=True)
+    year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
     slug = models.SlugField(default='', null=False, db_index=True)
 
